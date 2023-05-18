9eaeffdd7c2c64fbc7f3dd39ef2a1ae5d8713a49
LuciFull <116511131+LuciFull34220@users.noreply.github.com>
2023-05-18 14:59:28 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -5,6 +5,6 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {  
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write('Hello, this is Lucy! :D');
   res.end();
 }).listen(port);
